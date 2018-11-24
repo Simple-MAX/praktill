a6519ede476a3bfc5bd007a1b33d596b37eacc8f
MartinAk <matin@lifeappliance.cc>
2018-11-24 19:22:42 +0100

    landing page bug fix

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Praktill
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_controller.relative_url_root
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
